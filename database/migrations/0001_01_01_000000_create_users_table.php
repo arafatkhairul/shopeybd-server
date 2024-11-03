ac9ca1753b78ae721fdca1d2deb466be27bc95bf
Arafat Islam Khairul <108567092+arafatkhairul@users.noreply.github.com>
2024-11-03 21:14:28 +0600

    New updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('user_id')->unique();
+            $table->string('role')->default('user')->comment('admin | user');
             $table->string('user_name')->unique();
             $table->string('email')->unique()->nullable();
             $table->string('phone_number')->unique()->nullable();
